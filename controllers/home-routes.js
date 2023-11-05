e11c403789da2f131bee4ab630e8c64dc92810b4
Minami Itsukaichi <mitukaichi@gmail.com>
2023-11-04 21:44:37 -0700

    render contents to the homepage and modified css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,34 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
 
+// Get all posts for homepage
+router.get('/', async (req, res) => {
+    try {
+      const dbPostData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+      let posts = [];
+      for (i = 0; i < dbPostData.length; i++) {
+        posts.push(dbPostData[i].dataValues);
+        posts[i].username = dbPostData[i].dataValues.user.dataValues.username;
+      };
+      console.log(posts);
+      res.render('homepage', { posts });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+
+// Get a single post with comment for the post detail page
+
+
+// Get all posts you wrote in the dashboard page
+
+module.exports = router;
